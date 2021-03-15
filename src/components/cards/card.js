a17ea41fe022587a9311eada172fb43b32382ef6
gOHostwith1R <roslik2014@gmail.com>
2021-03-15 23:09:31 +0300

    check

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -1,17 +1,57 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect} from 'react';
 import './card.css'
 
+const Card = ( { cards, index, flipCard, onClickCard }) => {
 
-export default class Card extends Component {
+   /*const [matched, setMatched] = useState([]);
 
-    id = 100;
+   useEffect(() => {
+      const firstMach = cards[openCard[0]];
+      const secondMach = cards[openCard[1]];
 
-    render() {
-        const { cards } = this.props;
-        return cards.map((elem) => {
-           return (<img className='card'
-                       src={`images/cards/${elem}`}
-                       alt={elem} key={this.id++}/>)
-        });
-    };
+      if (secondMach && firstMach.id === secondMach.id) {
+        setMatched(() => [...matched, firstMach.id]);
+      }
+
+      if (openCard.length === 2) setTimeout(() => setOpenCard([]), 1000);
+
+   }, [cards, matched, openCard]);*/
+
+ /* const clickCard = (index) => {
+     setOpenCard((opened) => [...opened, index])
+
+   };*/
+
+   /*return (
+       <div className='cards'>
+       {cards.map((elem, index) => {
+          let flipCard;
+          flipCard = false;
+
+          if (openCard.includes(index)) flipCard = true;*/
+
+        //  if (matched.includes(elem.id)) flipCard = true;
+
+
+           return (
+               <div className={`card-card ${ flipCard ? 'flipped' : ''}`}
+                    onClick={() => onClickCard(index, cards.name)}
+                    key={index} >
+                   <div className='inner'>
+                       <div className='front' >
+                            <img className='image-card'
+                            src={`images/cards/${cards.image}`}
+                            alt={cards.name}
+                            />
+                        </div>
+                        <div className='back' > </div>
+                   </div>
+               </div>
+           );
+      /* })
+       }
+   </div>
+   );*/
 };
+
+export default Card;
