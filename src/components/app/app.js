0e16e729feea6514d700694c31104015f6394917
gOHostwith1R <roslik2014@gmail.com>
2021-03-18 23:23:58 +0300

    feat: add statistics

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
 
 import Card from "../cards";
 
@@ -6,6 +6,11 @@ import cardsData from "../../cards";
 import './app.css'
 
 const App = () => {
+
+    const [openCards, setOpen] = useState([]);
+    const [matched, setMatched] = useState([]);
+    let [counterMistakes, setCounterMistakes] = useState(0)
+    let [counterSuccessful, setCounterSuccessful] = useState(0);
 
     const newCards = [];
 
@@ -18,13 +23,13 @@ const App = () => {
     }
 
     shuffle();
-    const [cards] = useState(newCards);
-    const [openCards, setOpen] = useState([]);
-    const [matched, setMatched] = useState([]);
 
-    const onClickCard = (index) => {
+    const [cards] = useState(newCards);
+
+    const onCardClick = (index) => {
         setOpen((opened) => [...opened, index])
-    }
+    };
+
 
     useEffect(() => {
         const firstMach = cards[openCards[0]];
@@ -32,22 +37,32 @@ const App = () => {
 
         if (secondMach && firstMach.id === secondMach.id) {
             setMatched(() => [...matched, firstMach.id]);
+            setCounterSuccessful(counterSuccessful + 1);
         }
 
         if (openCards.length === 2) setTimeout(() => setOpen([]), 1000);
+        if (openCards.length === 2) setCounterMistakes(counterMistakes + 1);
 
-    }, [cards, matched, openCards]);
+    }, [openCards]);
 
         return (
             <div className='container' >
+                <div className='statistics'>
+                    <span className='counter'>Guessed pairs of card:{counterSuccessful}</span>
+                    <span className='counter-mistakes'>Counter mistakes:{counterMistakes}</span>
+                </div>
                 <div className='cards'>
                 {cards.map((elem,index) => {
                     let flipCard;
                     flipCard = openCards.includes(index);
                     if (matched.includes(elem.id)) flipCard = true;
-                 return <Card cards={elem} index={index} flipCard={flipCard} onClickCard={onClickCard} key={index}/>
+                 return <Card card={elem}
+                              index={index}
+                              flipCard={flipCard}
+                              onCardClick={onCardClick}
+                              key={index}/>
                 })
-                }
+                };
                 </div>
             </div>
         );
--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -1,57 +1,23 @@
 import React from 'react';
 import './card.css'
 
-const Card = ( { cards, index, flipCard, onClickCard }) => {
+const Card = ( { card, index, flipCard, onCardClick}) => {
 
-   /*const [matched, setMatched] = useState([]);
-
-   useEffect(() => {
-      const firstMach = cards[openCard[0]];
-      const secondMach = cards[openCard[1]];
-
-      if (secondMach && firstMach.id === secondMach.id) {
-        setMatched(() => [...matched, firstMach.id]);
-      }
-
-      if (openCard.length === 2) setTimeout(() => setOpenCard([]), 1000);
-
-   }, [cards, matched, openCard]);*/
-
- /* const clickCard = (index) => {
-     setOpenCard((opened) => [...opened, index])
-
-   };*/
-
-   /*return (
-       <div className='cards'>
-       {cards.map((elem, index) => {
-          let flipCard;
-          flipCard = false;
-
-          if (openCard.includes(index)) flipCard = true;*/
-
-        //  if (matched.includes(elem.id)) flipCard = true;
-
-
-           return (
-               <div className={`card-card ${ flipCard ? 'flipped' : ''}`}
-                    onClick={() => onClickCard(index, cards.name)}
-                    key={index} >
+    return (
+        <div className={`card-card ${ flipCard ? 'flipped' : ''}`}
+             onClick={() => onCardClick(index, card.name)}
+             key={index}>
                    <div className='inner'>
                        <div className='front' >
                             <img className='image-card'
-                            src={`images/cards/${cards.image}`}
-                            alt={cards.name}
+                            src={`images/cards/${card.image}`}
+                            alt={card.name}
                             />
                         </div>
                         <div className='back' > </div>
                    </div>
-               </div>
-           );
-      /* })
-       }
-   </div>
-   );*/
+        </div>
+    );
 };
 
 export default Card;
